a7996743a41623d435b545cefec9b586efff8fea
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-07-10 15:14:24 +0000

    Tidy up jenkins.sh
    Removed some redundant ENV variables.
    Set it to abort on error, avoids the need to explicitly exit with $?

--- jenkins.sh
+++ jenkins.sh
@@ -1,10 +1,9 @@
 #!/bin/bash -x
-export FACTER_govuk_platform=test
+
+set -e
+
 export RAILS_ENV=test
-export DISPLAY=":99"
 
 bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
 bundle exec rake stats
 bundle exec rake ci:setup:rspec default
-RESULT=$?
-exit $RESULT
